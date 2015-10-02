152f7cd6f647122dd5511190dbd6c0ddfcacba25
VariousMilkshakes <vm@hadesoft.io>
2015-10-02 15:34:26 +0100

    new tray icon

--- txtedo/Properties/Resources.Designer.cs
+++ txtedo/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace txtedo.Properties {
                 return ((System.Drawing.Icon)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon tuxicon {
+            get {
+                object obj = ResourceManager.GetObject("tuxicon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
     }
 }
